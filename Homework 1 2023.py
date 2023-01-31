1572c60449f22ebcab055f5bd47988af885293ad
Mark <malope34@cougarnet.uh.edu>
2023-01-31 16:55:50 -0600

    Base of Homework 1

---
+++ Homework 1 2023.py
@@ -0,0 +1,12 @@
+age = int
+
+print("Birthday Calculator")
+print("Current Day")
+print(int(input("Month: ")))
+print(int(input("Day:")))
+print(int(input("Year: ")))
+print("Birthday")
+print(int(input("Month: ")))
+print(int(input("Day:")))
+print(int(input("Year: ")))
+
