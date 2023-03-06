46e62811dabfbcf5063fc475fc16aa4322ec1850
Mark <malope34@cougarnet.uh.edu>
2023-03-05 20:41:00 -0600

    Homework 2 A.

--- Homework 2/Homework 2.py
+++ Homework 2/Homework 2.py
@@ -1,1 +1,15 @@
+import datetime
 
+current_date = datetime.datetime.now()
+
+while True:
+    date_str = input().strip()
+    if date_str == '-1':
+        break
+
+    try:
+        date = datetime.datetime.strptime(date_str, '%B %d, %Y')
+        if date <= current_date:
+            print(date.strftime('%m/%d/%Y'))
+    except ValueError:
+        pass
